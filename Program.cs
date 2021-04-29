7b3d12e1b6d2503cdddc3447b6ce3641db69c89a
mikekben <benmikek@gmail.com>
2021-04-29 14:36:59 -0500

    Getting started

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tree_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start of game");
+        }
+    }
+}
